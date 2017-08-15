a643327a30c107e0385ef269e307b3004c9d03d0
Bastiaan Laptop <deknudtb@gmail.com>
2017-08-15 23:02:06 +0200

    init

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+cd $(dirname $0);
+
+ln -sf $(pwd)/dot_vimrc ~/.vimrc
+
+mkdir -p ~/.vim/bundle
+# ln -sfn $(pwd)/Vundle.vim ~/.vim/bundle/vundle
+rm -rf ~/.vim/bundle/vundle;
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle;
+
+vim +PluginInstall +qall
+
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/swap
+
+ln -sf $(pwd)/dot_tmux.conf ~/.tmux.conf
+
+ln -sf $(pwd)/dot_eslintrc ~/.eslintrc
