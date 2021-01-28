aeee1d91c972f82559e8fc671d3ca01bec579f9f
Bastiaan Deknudt <deknudtb@gmail.com>
2021-01-28 20:37:01 +0100

    fix indent

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 function go () {
+  cd ~
   sudo apt update
   sudo apt install ansible git -y;
 
-  ansible-pull -U https://github.com/tionkje/dotfiles.git -d dotfiles ansible/playbook.yml;
+  ansible-pull -U https://github.com/tionkje/dotfiles.git -d ~/dotfiles ansible/playbook.yml;
 
   ~/dropbox.py update
   ~/dropbox.py status
