e66ff85b36df103e76357396e27743c5be6d5568
Bastiaan Deknudt <deknudtb@gmail.com>
2021-01-28 17:30:53 +0100

    ansible-pull

---
+++ ansible/README.md
@@ -0,0 +1,3 @@
+```console
+wget https://raw.githubusercontent.com/tionkje/dotfiles/master/ansible/setup.sh -q -O- | bash
+```
--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 function go () {
-  echo "hello";
+  sudo apt update
+  sudo apt install ansible -y;
+  ansible-pull -U https://github.com/tionkje/dotfiles.git -d dotfiles ansible/playbook.yml
 }
 go
