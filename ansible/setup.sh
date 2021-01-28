df90733461313ac75bd4b83a1948e725ed7ac930
Bastiaan Deknudt <deknudtb@gmail.com>
2021-01-28 20:26:09 +0100

    install dropbox

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 function go () {
   sudo apt update
-  sudo apt install ansible -y;
-  ansible-pull -U https://github.com/tionkje/dotfiles.git -d dotfiles ansible/playbook.yml
+  sudo apt install ansible git -y;
+
+  ansible-pull -U https://github.com/tionkje/dotfiles.git -d dotfiles ansible/playbook.yml;
+
+  ~/dropbox.py update
+  ~/dropbox.py status
 }
 go
