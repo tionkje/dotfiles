52893b0f94078a71ae32670f80e3c130f6422919
Bastiaan Deknudt <deknudtb@gmail.com>
2024-05-26 19:25:18 +0200

    add lsps

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -196,7 +196,19 @@ return { -- LSP Configuration & Plugins
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format Lua code
+			"ansiblels",
+			"eslint",
+			"eslint_d",
+			"lua_ls",
+			"nil_ls",
+			"nixpkgs-fmt",
+			"prettier",
+			"prettierd",
+			"rnix",
+			"stylua",
+			"svelte",
+			"tailwindcss",
+			"tsserver",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
