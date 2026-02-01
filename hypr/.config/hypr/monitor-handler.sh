345eaa6a77ba916d7eddb32a70961c087d98e7de
Bastiaan Deknudt <deknudtb@gmail.com>
2026-02-01 11:04:53 +0100

    moved hyprland/waybar/wleave and dunst config

---
+++ hypr/.config/hypr/monitor-handler.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+handle() {
+  # echo $1;
+  case $1 in
+    monitoraddedv2*);;
+    monitoradded*)
+      monitor=${1#monitoradded>>}
+      # echo "Monitor added: $monitor";
+      if [[ "$monitor" != "eDP-1" ]]; then
+        hyprctl dispatch moveworkspacetomonitor 1 "$monitor"
+        hyprctl dispatch moveworkspacetomonitor 2 "$monitor"
+        hyprctl dispatch moveworkspacetomonitor 3 "$monitor"
+        hyprctl dispatch moveworkspacetomonitor 4 "$monitor"
+        hyprctl dispatch moveworkspacetomonitor 5 "$monitor"
+        hyprctl dispatch moveworkspacetomonitor 6 "eDP-1"
+        hyprctl dispatch moveworkspacetomonitor 7 "eDP-1"
+      fi
+      ;;
+  esac
+}
+
+sleep 5
+for monitorname in $(hyprctl -j monitors | jq -r '.[].name'); do
+  handle "monitoradded>>$monitorname"
+done
+
+socat -U - UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do
+  handle "$line"
+done
+
